397973b1e2aacfd22705021098a1c702720c85ff
Ilya Korsunsky <ilya.korsunsky@gmail.com>
2018-05-16 14:14:33 -0400

    added basic methods for stochastic neighbor diversity method

---
+++ sndiversity/R/functions.R
@@ -0,0 +1,28 @@
+#' Function to compute diversity score for each data point
+#' @param X data matrix, samples (rows) by features (columns) (e.g. matrix(runif(500), 100, 5))
+#' @param batch_labels 1D array of discrete batch assignment (e.g. c("A", "A", "B", "C", "A", "B"))
+#' @param perplexity effective number of neighbors (defined in SNE/tSNE algorithms)
+#' @param ratio_use ratio of data to use. 1 by default. Faster with smaller ratio. 
+compute_batch_diversity <- function(X, batch_labels, perplexity = 30, ratio_use = 1) {
+  if (ratio_use < 1) {
+    samples_use <- sample.int(nrow(X), nrow(X) * ratio_use)
+    X <- X[samples_use, ]
+    batch_labels <- batch_labels[samples_use]
+  }
+
+  ## TODO: extend to non-discrete batch with P * Phi
+  dknn <- FNN::get.knn(X, k = 3 * perplexity)
+  batch_labels <- as.integer(factor(batch_labels)) - 1
+  n_batches <- length(unique(model$..data$batch_labels))
+  diversity <- compute_diversity_Cpp(t(dknn$nn.dist), t(dknn$nn.index) - 1, 
+                                     batch_labels, n_batches, perplexity)
+
+  ## compute maximal theoretical diversity
+  max_diversity <- 1 - (n_batches * ((1/n_batches) ^ 2))
+
+  ## scale diversity from 0 to 1
+  diversity <- diversity - min(diversity)
+  diversity <- diversity / max_diversity
+  return(diversity)
+}
+
---
+++ sndiversity/src/diversity.cpp
@@ -0,0 +1,67 @@
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+#include <cmath>
+
+using namespace Rcpp;
+using namespace arma;
+// [[Rcpp::depends(RcppArmadillo)]]
+
+float Hbeta(arma::mat& D, float beta, arma::vec& P, int idx) {
+  P = arma::exp(-D.col(idx) * beta);
+  float sumP = sum(P);
+  float H;
+  if (sumP == 0){
+      H = 0;
+      P = D.col(idx) * 0;
+  } else {
+      H = log(sumP) + beta * sum(D.col(idx) % P) / sumP;
+      P /= sumP;
+  }
+  return(H);
+}
+
+// [[Rcpp::export]]
+arma::vec compute_diversity_Cpp(arma::mat& D, arma::umat& knn_idx, arma::vec& batch_labels, int n_batches,
+                float perplexity = 15, float tol = 1e-5) {
+  int n = D.n_cols;
+  arma::vec P = zeros<arma::vec>(D.n_rows);
+  arma::vec diversity = ones<arma::vec>(n);
+  float logU = log(perplexity);
+
+  float hbeta, beta, betamin, betamax, H, Hdiff;
+  int tries;
+  for (int i = 0; i < n ; i++) {
+    beta = 1;
+    betamin = -datum::inf;
+    betamax = datum::inf;
+    H = Hbeta(D, beta, P, i);
+    Hdiff = H - logU;
+    tries = 0;
+    // first get neighbor probabilities
+    while(std::abs(Hdiff) > tol && tries < 50) {
+      if (Hdiff > 0){
+        betamin = beta;
+        if (!is_finite(betamax)) beta *= 2;
+        else beta = (beta + betamax) / 2;
+      } else{
+        betamax = beta;
+        if (!is_finite(betamin)) beta /= 2;
+        else beta = (beta + betamin) / 2;
+      }
+
+      H = Hbeta(D, beta, P, i);
+      Hdiff = H - logU;
+      tries++;
+    }
+
+    // then compute diversity
+    for (int b = 0; b < n_batches; b++) {
+      uvec q = find(batch_labels.elem(knn_idx.col(i)) == b); // indices of cells belonging to batch (b)
+      if (q.n_elem > 0) {
+        float sumP = sum(P.elem(q));      
+        diversity.row(i) -= sumP * sumP;         
+      }
+    }
+  }
+  return(diversity);
+}
